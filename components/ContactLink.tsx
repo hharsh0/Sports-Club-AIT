ec1e5efab9f7bcf529cda7b919eeb8a82fc47d9a
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-28 17:57:20 +0530

    changing meta data

--- components/ContactLink.tsx
+++ components/ContactLink.tsx
@@ -9,7 +9,7 @@ function ContactLink({ title, href, icon }:props) {
     <>
       <a href={href} target="_blank" rel="noopener noreferrer">
         <li className="duration-250 mr-2 inline cursor-pointer text-4xl transition-colors ease-in hover:text-gray-400 sm:text-6xl md:text-5xl lg:text-6xl">
-          <span className="duration-250 hover:text-primary-500 dark:hover:text-primary-400 transition-colors ease-in">
+          <span className="duration-250  transition-colors ease-in">
             {icon}
           </span>
           <span className="font-light opacity-50">@</span>
--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -6,9 +6,9 @@ const DOMAIN = "";
 
 
 export default function Meta({
-  title = "Precedent - Building blocks for your Next.js project",
-  description = "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  image = `${DOMAIN}/api/og`,
+  title = "Ait Sports club",
+  description = "Ait sports club website",
+  image = ``,
 }: {
   title?: string;
   description?: string;
@@ -32,10 +32,9 @@ export default function Meta({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta itemProp="image" content={image} />
-      <meta property="og:logo" content={`${DOMAIN}/logo.png`}></meta>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      {/* <meta property="og:image" content={image} /> */}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@vercel" />
