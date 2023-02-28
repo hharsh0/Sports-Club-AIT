b845a8d98406b5eed002d3397938fd5e3e2a426d
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-28 17:30:52 +0530

    contacts page layout update

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,11 +4,11 @@ import ContactLink from '@/components/ContactLink';
 function Contact() {
   return (
     <>
-      <div className="relative max-w-3xl overflow-hidden">
-        <h1 className="md:leading-14 text-3xl font-medium leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl">
-          Contacts
-        </h1>
+      <div className="relative my-10 flex w-screen flex-col items-center">
         <div className="animate-[slide-down-fade_0.9s_ease-in-out] pt-10 pb-8">
+          <h1 className="md:leading-14 mb-10 text-3xl font-medium leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl">
+            Contacts
+          </h1>
           <ul className="font-semi-bold flex flex-col space-y-4">
             <ContactLink
               href="mailto:harsh.v7681@gmail.com"
