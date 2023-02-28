41815bda48be677a6e3820e4cef76e6d8e210b53
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-28 16:51:23 +0530

    footer change

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -101,17 +101,6 @@ export default function Layout({
         {children}
       </main>
       <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
-        {/* <p className="text-gray-500">
-          A free template by{" "}
-          <a
-            className="font-medium text-gray-800 underline transition-colors"
-            href="https://twitter.com/steventey"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Steven Tey
-          </a>
-        </p> */}
         <Footer />
       </div>
     </>
