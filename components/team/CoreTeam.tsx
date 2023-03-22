d3319c0ad884938b9e77a81bc21945fa8c4ff72f
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-03-22 17:04:47 +0530

    add core team members

--- components/team/CoreTeam.tsx
+++ components/team/CoreTeam.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import TeamCard from '../TeamCard';
-import Image from 'next/image';
+import React from "react";
+import TeamCard from "../TeamCard";
+import Image from "next/image";
 
 function CoreTeam() {
   return (
@@ -14,7 +14,7 @@ function CoreTeam() {
             <>
               <Image
                 alt="contact"
-                src="/team/core/arajeet.jpeg"
+                src="/team/core/sec/arajeet.jpeg"
                 width={200}
                 height={200}
               />
@@ -22,14 +22,46 @@ function CoreTeam() {
           }
         />
         <TeamCard
-          title="Arajeet Pandey"
+          title="Dasari Hema Sree"
           description="Sports Secretary"
           large={false}
           demo={
             <>
               <Image
                 alt="contact"
-                src="/team/core/arajeet.jpeg"
+                src="/team/core/sec/hema.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <br />
+        <TeamCard
+          title="P Raju"
+          description="TE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/temembers/raju.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+
+        <TeamCard
+          title="Shiv raj"
+          description="TE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/temembers/shivraj.jpg"
                 width={200}
                 height={200}
               />
@@ -37,14 +69,14 @@ function CoreTeam() {
           }
         />
         <TeamCard
-          title="Arajeet Pandey"
-          description="Sports Secretary"
+          title="Fahad Faizan"
+          description="TE Member"
           large={false}
           demo={
             <>
               <Image
                 alt="contact"
-                src="/team/core/arajeet.jpeg"
+                src="/team/core/temembers/fahad.jpg"
                 width={200}
                 height={200}
               />
@@ -52,14 +84,14 @@ function CoreTeam() {
           }
         />
         <TeamCard
-          title="Arajeet Pandey"
-          description="Sports Secretary"
+          title="Sahil Chahal"
+          description="TE Member"
           large={false}
           demo={
             <>
               <Image
                 alt="contact"
-                src="/team/core/arajeet.jpeg"
+                src="/team/core/temembers/SAHIL.jpeg"
                 width={200}
                 height={200}
               />
@@ -67,14 +99,30 @@ function CoreTeam() {
           }
         />
         <TeamCard
-          title="Arajeet Pandey"
-          description="Sports Secretary"
+          title="Himanshu Pawar"
+          description="TE Member"
           large={false}
           demo={
             <>
               <Image
                 alt="contact"
-                src="/team/core/arajeet.jpeg"
+                src="/team/core/temembers/HIMANSHU.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+
+        <TeamCard
+          title="Basant Kumar"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/basant.jpg"
                 width={200}
                 height={200}
               />
@@ -82,23 +130,220 @@ function CoreTeam() {
           }
         />
         <TeamCard
-          title="Arajeet Pandey"
-          description="Sports Secretary"
+          title="Mayank Upadhyay"
+          description="Jt Secretary"
           large={false}
           demo={
             <>
               <Image
                 alt="contact"
-                src="/team/core/arajeet.jpeg"
+                src="/team/core/jtsec/MAYANK.jpg"
                 width={200}
                 height={200}
               />
             </>
           }
         />
+        <TeamCard
+          title="Allam Raju"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/ALLAM.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Madhur Mohit"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/MADUR.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Mohit"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/MOHIT.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Vikas Saran"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/saran.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Nisha Yadav"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/nisha.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Vandana Kandpal"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/VANDANA.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Aanchal Kumari"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/ANCHAL.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Akansha Dhami"
+          description="Jt Secretary"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/akansha.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+
+        <TeamCard
+          title="GV SAI Sisindra"
+          description="SE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/saii.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Jyoti Kumari"
+          description="SE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/JYOTI.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+
+        <TeamCard
+          title="Ayush Bhadouria"
+          description="SE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/AAYUSH.jpeg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        <TeamCard
+          title="Likhith Reddy"
+          description="SE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/lik.jpg"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        />
+        {/* <TeamCard
+          title="Himanshu Rai"
+          description="SE Member"
+          large={false}
+          demo={
+            <>
+              <Image
+                alt="contact"
+                src="/team/core/jtsec/p"
+                width={200}
+                height={200}
+              />
+            </>
+          }
+        /> */}
       </div>
     </>
   );
 }
 
-export default CoreTeam
+export default CoreTeam;
