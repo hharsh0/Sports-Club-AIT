d00eb3030b6f1afaf815b3b24110f4d0ab91ce42
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-03-22 21:56:26 +0530

    add socials

--- components/TeamCard.tsx
+++ components/TeamCard.tsx
@@ -12,11 +12,15 @@ export default function TeamCard({
   description,
   demo,
   large,
+  instagram,
+  linkedin
 }: {
   title: string;
   description: string;
   demo: ReactNode;
   large?: boolean;
+  instagram?: string;
+  linkedin?: string;
 }) {
   return (
     <div
@@ -29,8 +33,8 @@ export default function TeamCard({
         <h2 className="mt-4 bg-gradient-to-br from-black to-stone-500 bg-clip-text font-display text-xl font-bold text-transparent md:text-3xl md:font-normal">
           <Balancer>{title}</Balancer>
         </h2>
-        <div className="prose-sm w-full -mt-2 px-4 leading-normal text-gray-500 md:prose">
-          <div className='px-8'>
+        <div className="prose-sm -mt-2 w-full px-4 leading-normal text-gray-500 md:prose">
+          <div className="px-8">
             <ReactMarkdown
               components={{
                 a: ({ node, ...props }) => (
@@ -38,7 +42,7 @@ export default function TeamCard({
                     target="_blank"
                     rel="noopener noreferrer"
                     {...props}
-                    className="font-medium text-gray-800 w-full underline transition-colors"
+                    className="w-full font-medium text-gray-800 underline transition-colors"
                   />
                 ),
                 code: ({ node, ...props }) => (
@@ -53,9 +57,13 @@ export default function TeamCard({
             >
               {description}
             </ReactMarkdown>
-            <div className="flex items-center w-full shrink-0 justify-between py-4">
-              <InstagramLogoIcon className="h-6 w-6" />
-              <LinkedInLogoIcon className="h-6 w-6" />
+            <div className="flex w-full shrink-0 items-center justify-between py-4">
+              <a target="_blank" href={instagram}>
+                <InstagramLogoIcon className="h-6 w-6 cursor-pointer" />
+              </a>
+              <a target="_blank" href={linkedin}>
+                <LinkedInLogoIcon className="h-6 w-6 cursor-pointer" />
+              </a>
             </div>
           </div>
         </div>
