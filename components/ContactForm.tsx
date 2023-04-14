d9ed14a75370b50548b3bafcc8fb187c526a0b1f
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-04-14 18:15:06 +0530

    contact us form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import {useState} from 'react'
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
 
 function ContactForm() {
+    const [formData, setFormData] = useState<FormData>({
+      name: "",
+      email: "",
+      message: "",
+    });
+
+     const handleChange = (
+       e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+     ) => {
+       setFormData({ ...formData, [e.target.name]: e.target.value });
+     };
+
     const handleSubmit = (event:any) => {
         event.preventDefault();
+        console.log("submitted")
+        alert("This feature is not yet implemented")
+    //     fetch("/api/send-email", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify(formData),
+    //     }).then((res) => {
+    //       console.log("Response received");
+    //       if (res.status === 200) {
+    //         console.log("Response succeeded!");
+    //       }
+    //     });
     }
   return (
     <>
@@ -12,9 +44,7 @@ function ContactForm() {
         id="form"
         className="needs-validation"
       >
-        <input
-          type="hidden"
-        />
+        <input type="hidden" />
         <input
           type="checkbox"
           className="hidden"
@@ -27,6 +57,8 @@ function ContactForm() {
             placeholder="Full Name"
             className="w-full rounded-md border-2 border-gray-300 px-4 py-3 outline-none ring-gray-100 placeholder:text-gray-800 focus:border-gray-600 focus:ring-4"
             name="name"
+            onChange={handleChange}
+            value={formData.name}
           />
           {/* <div className="empty-feedback invalid-feedback mt-1 text-sm text-red-400">
             Please provide your full name.
@@ -41,6 +73,8 @@ function ContactForm() {
             type="email"
             placeholder="Email Address"
             name="email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full rounded-md border-2 border-gray-300 px-4 py-3 outline-none  ring-gray-100 placeholder:text-gray-800 focus:border-gray-600 focus:ring-4"
           />
           {/* <div className="empty-feedback mt-1 text-sm text-red-400">
@@ -56,13 +90,15 @@ function ContactForm() {
             placeholder="Your Message"
             className="h-36 w-full rounded-md border-2 border-gray-300 px-4 py-3 outline-none ring-gray-100 placeholder:text-gray-800 focus:border-gray-600 focus:ring-4"
             defaultValue={""}
+            value={formData.message}
+            onChange={handleChange}
           />
           {/* <div className="empty-feedback invalid-feedback mt-1 text-sm text-red-400">
             Please enter your message.
           </div> */}
         </div>
         <button
-          className="w-full rounded border-2 border-transparent bg-black lg:px-6 py-3 text-center text-white ring-gray-200 ring-offset-2 transition hover:bg-slate-900 focus-visible:ring-2 md:px-5 md:py-2.5"
+          className="w-full rounded border-2 border-transparent bg-black py-3 text-center text-white ring-gray-200 ring-offset-2 transition hover:bg-slate-900 focus-visible:ring-2 md:px-5 md:py-2.5 lg:px-6"
           type="submit"
         >
           Send Message
